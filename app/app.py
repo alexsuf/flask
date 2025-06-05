6eaffe5371e07a1dab8d3603c8ebc06311531cc3
alexsuf <aleksey.zadonsky@gmail.com>
2025-06-05 17:12:02 +0300

    Сделан редирект (исправление обновления страницы)

--- app/app.py
+++ app/app.py
@@ -1,11 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import psycopg2
 from pprint import pprint
-import time
 
 app = Flask(__name__, template_folder='templates')
 
-# Подключение к базе данных
 conn = psycopg2.connect(
     host="host.docker.internal",
     port='1111',
@@ -15,38 +13,26 @@ conn = psycopg2.connect(
 )
 cur = conn.cursor()
 
-# Определение маршрута для вывода списка задач
 @app.route('/')
 def index():
-    # Выборка всех задач из таблицы public.app_tasks
-#    cur.execute("SELECT id, task FROM public.app_tasks")
-    cur.execute("select id, CONCAT(' (', TO_CHAR(dtime, 'DD.MM.YYYY'), ') --> ', task) from public.app_tasks")
+    cur.execute("SELECT id, CONCAT(' (', TO_CHAR(dtime, 'DD.MM.YYYY'), ') --> ', task) FROM public.app_tasks")
     rows = cur.fetchall()
     pprint(rows)
-    # Вывод списка задач на экран
     return render_template('index.html', rows=rows)
 
-# Определение маршрута для удаления задачи
 @app.route('/delete', methods=['POST'])
 def delete():
-    # Получение id задачи из запроса
     task_id = request.form['task_id']
-#    print ('task_id = ', task_id)
-    # Удаление задачи из таблицы public.app_tasks
     cur.execute("DELETE FROM public.app_tasks WHERE id = %s", (task_id,))
     conn.commit()
-    return index()
+    return redirect(url_for('index'))  # ✅ Редирект вместо возврата index()
 
-# Определение маршрута для добавления задачи
 @app.route('/add', methods=['POST'])
 def add():
-    # Получение текста задачи из запроса
     task_text = request.form['task_text']
-    # Добавление задачи в таблицу public.app_tasks
     cur.execute("INSERT INTO public.app_tasks (task) VALUES (%s)", (task_text,))
     conn.commit()
-    # Перенаправление на главную страницу
-    return index()
+    return redirect(url_for('index'))  # ✅ Редирект вместо возврата index()
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
